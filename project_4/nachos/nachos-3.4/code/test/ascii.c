0e9acacdcefd1ce0f23d719ef737fdbc9aa942fa
20120443 <nguyenchuaqna@gmail.com>
2022-11-30 13:47:26 +0700

    .

---
+++ project_4/nachos/nachos-3.4/code/test/ascii.c
@@ -0,0 +1,22 @@
+#include "syscall.h"
+
+int main()
+{
+    char item = 0;
+    int i;
+
+    printString("--------ASCII TABLE--------\n");
+
+    for(i = 0; i < 255; i++)
+    {
+        printInt(i);
+        printString(" - ");
+        printChar(item);
+        printString("\t");
+	if(i % 6 == 0)
+            printString("\n");
+	item++;
+    }
+    Halt();
+    return 0;
+}

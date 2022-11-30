b03c65cbf8d061c068f9b721f85ef4d3c73537ec
20120443 <nguyenchuaqna@gmail.com>
2022-11-30 13:26:32 +0700

    update test

--- project_3/source/nachos/nachos-3.4/code/test/main.c
+++ project_3/source/nachos/nachos-3.4/code/test/main.c
@@ -8,58 +8,58 @@ main()
     char c[1];
     int n, ret;
     SpaceId svproc;
-    ret = CreateF("output.txt");
-    if (ret == -1) ExitProc(-1);
+    ret = CreateFile("output.txt");
+    if (ret == -1) Exit(-1);
     ret = CreateSemaphore("sinhvien", 0);
-    if (ret == -1) ExitProc(-1);
+    if (ret == -1) Exit(-1);
     ret = CreateSemaphore("voinuoc", 0);
-    if (ret == -1) ExitProc(-1);
-    inp = OpenF("input.txt", 1);
-    if (inp == -1) ExitProc(-1);
-    out = OpenF("output.txt", 0);
+    if (ret == -1) Exit(-1);
+    inp = Open("input.txt", 1);
+    if (inp == -1) Exit(-1);
+    out = Open("output.txt", 0);
     if (out == -1) {
-        CloseF(inp);
+        Close(inp);
         ExitProc(-1);
     }
     n = 0;
     while (1) {
-        if (ReadF(c, 1, inp) == 0) break;
+        if (Read(c, 1, inp) == 0) break;
         if (c[0] >= '0' && c[0] <= '9') n = 10 * n + c[0] - '0';
         else if (c[0] == '\n') break;
     }
     while (n--) {
-        ret = CreateF("sinhvien.txt");
+        ret = CreateFile("sinhvien.txt");
         if (ret == -1) {
-            CloseF(inp);
-            ExitProc(-1);
+            Close(inp);
+            Exit(-1);
         }
-        sv = OpenF("sinhvien.txt", 0);
+        sv = Open("sinhvien.txt", 0);
         if (sv == -1) {
-            CloseF(inp);
-            ExitProc(-1);
+            Close(inp);
+            Exit(-1);
         }
         while (1) {
-            if (ReadF(c, 1, inp) == 0) break;
-            WriteF(c, 1, sv);
+            if (Read(c, 1, inp) == 0) break;
+            Write(c, 1, sv);
             if (c[0] == '\n') break;
         }
-        CloseF(sv);
-        svproc = ExecProc("test/sinhvien");
-        if (svproc != -1) ret = JoinProc(svproc);
+        Close(sv);
+        svproc = Exec("test/sinhvien");
+        if (svproc != -1) ret = Join(svproc);
         if (ret == -1) {
-            CloseF(inp);
-            CloseF(out);
-            ExitProc(-1);
+            Close(inp);
+            Close(out);
+            Exit(-1);
         }
-        res = OpenF("result.txt", 0);
+        res = Open("result.txt", 0);
         if (res == -1) {
-            CloseF(inp);
+            Close(inp);
             CloseF(out);
-            ExitProc(-1);
+            Exit(-1);
         }
-        while (ReadF(c, 1, res) != 0)
-            WriteF(c, 1, out);
-        CloseF(res);
+        while (Read(c, 1, res) != 0)
+            Write(c, 1, out);
+        Close(res);
     }
-    ExitProc(0);
+    Exit(0);
 }
